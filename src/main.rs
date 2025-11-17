e92e500ff0fc6cf2054a45596d4a0f997c3a713b
Alexis Massa <alexis.massa-externe@biologic.net>
2025-11-17 15:01:03 +0100

    chore: Can count files in folder

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::{error::Error, path::PathBuf};
+use walkdir::WalkDir;
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Folder to discover files from
+    #[arg()]
+    folder: PathBuf,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+    let file_count: usize = WalkDir::new(&args.folder)
+        .into_iter()
+        .collect::<Vec<Result<walkdir::DirEntry, walkdir::Error>>>()
+        .into_iter()
+        .filter(|dir| dir.is_ok())
+        .collect::<Vec<Result<walkdir::DirEntry, walkdir::Error>>>()
+        .len();
+    println!("{} files in {}!", file_count, args.folder.display());
+    Ok(())
 }
